5ecfd0e5b1725689537cb40874a5a656b9010d6e
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-10-03 23:27:06 +0600

    signUp and route setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login';
+import { Link, Route, Routes } from 'react-router-dom';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
     <div className="App">
-     <p>messenger</p>
-     <p>isa</p>
-     <p>my wrllksdkj</p>
-     <p>hello world</p>
-    <span>pull request from kawsar</span>
+      <Link to="/login">Login</Link>
+      <Link to="/signUp">Signup</Link>
+     <Routes>
+      <Route path='login' element={<Login></Login>}></Route>
+      <Route path='signUp' element={<SignUp></SignUp>}></Route>
+     </Routes>
      
     </div>
   );
