bfe72ea907e35bed15cf56a592ec1f7623980ca3
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-10-03 21:37:59 +0600

    install tailwindCss

--- src/App.js
+++ src/App.js
@@ -4,15 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     <p>messenger</p>
-     <p>isa</p>
-     <div>
-      <span>git setup has been completed</span>
-      <span>git setup has been completed</span>
-      <span>git setup has been completed</span>
-      <span>git setup has been completed</span>
-      <span>git setup has been completed</span>
-     </div>
+   
      
     </div>
   );
