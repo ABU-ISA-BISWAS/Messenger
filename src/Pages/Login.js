0d856c564abc01ed5d5e4baff80ad48233c89ada
ABU-ISA-BISWAS <bmabuisa.cse@gmail.com>
2022-10-21 01:39:14 +0600

    search user and chat with him

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,31 +1,54 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
 
 
 const Login = () => {
-    return (
-     
-        <div className="formContainer ">
-        <div className="formWrapper">
-            <span className="logo font-sans" >My chat</span>
-            
-            <form>
-                
-               
-                <input className='p-1 rounded-sm border-blue-500 border-2' type="email" placeholder="email"/>
-             
-                <input className='p-1 rounded-sm border-blue-500 border-2 input-xs' type="password" placeholder="password"  />
-              
-                
-                
-                <button className='w-full bg-blue-600 rounded-sm p-2 text-white font-semibold'>Login</button>
-      
+    const [err , setErr] = useState(false);
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
 
-            </form>
-            <p>don't you have an account ? <Link to="/signUp">Register</Link></p>
+    const handleSubmit = async (e) => {
+        setLoading(true);
+        e.preventDefault();
+
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+
+
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            navigate('/');
+
+        } catch (err) {
+            setErr(true);
+            setLoading(false);
+        }
+    };
+    return (
+
+        <div className="formContainer ">
+            <div className="formWrapper">
+                <span className="logo font-sans" >My chat</span>
+
+                <form onSubmit={handleSubmit}>
+
+
+                    <input className='p-1 rounded-sm border-blue-500 border-2' type="email" placeholder="email" />
+
+                    <input className='p-1 rounded-sm border-blue-500 border-2 input-xs' type="password" placeholder="password" />
+
+
+
+                    <button className='w-full bg-blue-600 rounded-sm p-2 text-white font-semibold'>Login</button>
+
+
+                </form>
+                <p>don't you have an account ? <Link to="/signUp">Register</Link></p>
+            </div>
         </div>
-    </div>
-       
+
     );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {signOut} from "firebase/auth"
 import { auth } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const Navbar = () => {
+    const {currentUser} = useContext(AuthContext);
     return (
         <div className='navbar'>
             <span className='logo'>My Chat</span>
             <div className="user">
-                <img src="https://i.ibb.co/Dt7DTk5/bg.jpg" alt="" />
-                <span>John</span>
-                <button onClick={()=>signOut(auth)}>logout</button>
+                <img src={currentUser.photoURL} alt="" />
+                <span>{currentUser.displayName}</span>
+                <button className='rounded px-2' onClick={()=>signOut(auth)}>logout</button>
             </div>
         </div>
     );
